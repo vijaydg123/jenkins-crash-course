a68c28bfcb5ac8b76ec7fa51d51490e10d8cac68
vijaydg123 <vijay254gudaganavar@gmail.com>
2024-02-27 21:01:15 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+
+public class Main{
+
+
+public static void main(String[] args) {
+    System.out.println("This is java jenkins programm");
+    System.out.println("How are you");
+    System.out.println(new Date());
+}
+}
